d6f690720cbb251b2da3597db816a2604378189c
Ganeral_program <vishalrajputvr9592057@gmail.com>
2026-02-11 13:31:36 +0530

    Initial python program

---
+++ 5th_program.py
@@ -0,0 +1,27 @@
+a = "vishalrajput"
+b = True
+c = 3.14
+d = None
+
+print(a)
+print(b)
+print(c)
+print(d)
+print("The type of a is",type(a))
+print("The type of b is",type(b))
+print("The type of c is",type(c))
+print("The type of d is",type(d))
+
+list1 = [0,1,22,52,[1,2,3], "vishal", 3.14, True, None,[ "banana", "apple", "grapes"]]
+print(list1)
+print(type(list1))
+
+tuple1 = (0,1,22,52,[1,2,3], "vishal", 3.14, True, None,[ "banana", "apple", "grapes"])
+print(tuple1)
+print(type(tuple1))
+tuple = (("vishal", "rajput"), ("python", "programmer"))
+print(tuple)
+
+dict1 = {"name":"vishal", "age": 20, "course": "python", "is_adult": True, "hobbies": ["coding", "gaming", "cooking"]}
+print(dict1)
+print(type(dict1))
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+a = input("enter your name: ")
+# print("my name is ", a)
+
+# b = input("enter your age: ")
+# print("my age is ", b)
+
+# c  = input("enter your course: ")
+# print("my course is ", c)
+
+# d = input("enter your hobbies: ")
+# print("my hobbies are ", d)
