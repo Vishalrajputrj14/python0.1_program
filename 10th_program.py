591df54910460ad7878111140c70af22be3af78e
Ganeral_program <vishalrajputvr9592057@gmail.com>
2026-02-13 13:28:44 +0530

    add a new file in python

---
+++ 10th_program.py
@@ -0,0 +1,10 @@
+names = "vishalrajput"
+print(names[0:3])
+print(len(names))
+fruit = "Mango"
+len1 = len(fruit)
+print("Mango is a ", len1, "letter word")
+print(fruit[0:len(fruit)-3])
+
+nm = "Harry"
+print(nm[-4:-2])
