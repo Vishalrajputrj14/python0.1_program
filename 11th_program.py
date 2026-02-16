44b549fed4128717fad975bfaf03ddb3d9b98d82
Ganeral_program <vishalrajputvr9592057@gmail.com>
2026-02-16 14:17:00 +0530

    add a new line

---
+++ 11th_program.py
@@ -0,0 +1,19 @@
+#  string are immutable in python
+name = "vishal   !!!!!!!!!!!!!!!  vishal"
+
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.title())
+print(name.count("a"))
+print(name.rstrip("l") )
+print(name.replace("vishal", "sonam"))
+print(name.split("  "))
+blogheanding = "python is a great programming language"
+print(blogheanding.capitalize())
+print(name.count("vishalrajput!!!!!!!!!!!!!!!  vishalrajput  "))
+str =  " Welcome to the Console !!!"
+print(str.endswith("!!!"))
+
+
