a0682a17c61c0cc204c3dd47633a1f4eabbdcdac
Ganeral_program <vishalrajputvr9592057@gmail.com>
2026-02-16 15:05:44 +0530

    add a new line

--- 11th_program.py
+++ 11th_program.py
@@ -16,4 +16,16 @@ print(name.count("vishalrajput!!!!!!!!!!!!!!!  vishalrajput  "))
 str =  " Welcome to the Console !!!"
 print(str.endswith("!!!"))
 
+str1 = "He's name is Dan. He is an honest man.  "
+print(str1.find("ishh"))
+# print(str1.index("ishh"))
 
+str2 = "WelcomeToTheconsole"
+print(str2.isalnum())
+str3 = "Welcome to the console"
+print(str3.isalpha())
+
+str4 = "hello world"
+print(str4.islower())
+str5 = "HELLO WORLD"
+print(str5.isupper())
