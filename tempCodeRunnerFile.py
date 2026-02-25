7924a1471e7cd991a9413140db214d445bce003a
Ganeral_program <vishalrajputvr9592057@gmail.com>
2026-02-25 12:32:10 +0530

    add a new file with while loop

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,13 +1,4 @@
-name = "vishal"
-# for i in name:
-#     print(i)
 
-# colurs = ["red", "green", "blue", "yellow"]
-# for colour in colurs:
-#     print(colour)
-
-# for i in range(1, 100):
-#     print(i)
-
-# for i in range(2, 21, 2):
-#     print(i)
+# num1 = int(input("Enter a number : "))
+# for i in range(1,11):
+#     print(f"{num1} x {i} = {num1*i}")
